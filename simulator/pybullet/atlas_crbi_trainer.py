2653b46f42b7f4f8d02b66ca356864e973cd8523
shbang91 <bangsh0718@gmail.com>
2022-04-20 18:40:44 -0500

    working on ik for training atlas crbi model

--- simulator/pybullet/atlas_crbi_trainer.py
+++ simulator/pybullet/atlas_crbi_trainer.py
@@ -11,6 +11,14 @@ from util.python_utils import interpolation
 from util.python_utils import util
 from util.python_utils import liegroup
 
+## Configs
+VIDEO_RECORD = False
+PRINT_FREQ = 10
+DT = 0.01
+PRINT_ROBOT_INFO = False
+INITIAL_POS_WORLD_TO_BASEJOINT = [0, 0, 1.5 - 0.761]
+INITIAL_QUAT_WORLD_TO_BASEJOINT = [0., 0., 0., 1.]
+
 #motion boundary
 SWING_TIME_LB, SWING_TIME_UB = 0.35, 0.7
 
@@ -25,6 +33,27 @@ FOOT_EA_LB = np.array([np.deg2rad(-5.), np.deg2rad(-15.), np.deg2rad(-45)])
 FOOT_EA_UB = np.array([np.deg2rad(5.), np.deg2rad(15.), np.deg2rad(45)])
 
 BASE_HEIGHT_LB, BASE_HEIGHT_UB = 0.7, 0.8
+
+
+def set_initial_config(robot, joint_id):
+    # shoulder_x
+    p.resetJointState(robot, joint_id["l_arm_shx"], -np.pi / 4, 0.)
+    p.resetJointState(robot, joint_id["r_arm_shx"], np.pi / 4, 0.)
+    # elbow_y
+    p.resetJointState(robot, joint_id["l_arm_ely"], -np.pi / 2, 0.)
+    p.resetJointState(robot, joint_id["r_arm_ely"], np.pi / 2, 0.)
+    # elbow_x
+    p.resetJointState(robot, joint_id["l_arm_elx"], -np.pi / 2, 0.)
+    p.resetJointState(robot, joint_id["r_arm_elx"], -np.pi / 2, 0.)
+    # hip_y
+    p.resetJointState(robot, joint_id["l_leg_hpy"], -np.pi / 4, 0.)
+    p.resetJointState(robot, joint_id["r_leg_hpy"], -np.pi / 4, 0.)
+    # knee
+    p.resetJointState(robot, joint_id["l_leg_kny"], np.pi / 2, 0.)
+    p.resetJointState(robot, joint_id["r_leg_kny"], np.pi / 2, 0.)
+    # ankle
+    p.resetJointState(robot, joint_id["l_leg_aky"], -np.pi / 4, 0.)
+    p.resetJointState(robot, joint_id["r_leg_aky"], -np.pi / 4, 0.)
 
 
 def sample_locomotion_boundary(nominal_lf_iso, nominal_rf_iso,
@@ -79,6 +108,97 @@ def sample_locomotion_boundary(nominal_lf_iso, nominal_rf_iso,
     return initial_base_iso, final_base_iso, initial_lf_iso, mid_lf_iso, final_lf_iso, mid_lf_vel, initial_rf_iso, mid_rf_iso, final_rf_iso, mid_rf_vel, swing_time
 
 
+def generate_locomotion_trajectories(initial_base_iso, final_base_iso,
+                                     initial_lf_iso, mid_lf_iso, final_lf_iso,
+                                     mid_lf_vel, initial_rf_iso, mid_rf_iso,
+                                     final_rf_iso, mid_rf_vel, swing_time):
+
+    base_pos_curve = interpolation.HermiteCurveVec(initial_base_iso[0:3, 3],
+                                                   np.zeros(3),
+                                                   final_base_iso[0:3, 3],
+                                                   np.zeros(3), swing_time)
+
+    base_quat_curve = interpolation.HermiteCurveQuat(
+        util.rot_to_quat(initial_base_iso[0:3, 0:3]), np.zeros(3),
+        util.rot_to_quat(final_base_iso[0:3, 0:3]), np.zeros(3), swing_time)
+
+    lf_pos_curve_first_half = interpolation.HermiteCurveVec(
+        initial_lf_iso[0:3, 3], np.zeros(3), mid_lf_iso[0:3, 3], mid_lf_vel,
+        swing_time)
+
+    lf_pos_curve_second_half = interpolation.HermiteCurveVec(
+        mid_lf_iso[0:3, 3], mid_lf_vel, final_lf_iso[0:3, 3], np.zeros(3),
+        swing_time)
+
+    lf_quat_curve = interpolation.HermiteCurveQuat(
+        util.rot_to_quat(initial_lf_iso[0:3, 0:3]), np.zeros(3),
+        util.rot_to_quat(final_lf_iso[0:3, 0:3]), np.zeros(3), swing_time)
+
+    rf_pos_curve_first_half = interpolation.HermiteCurveVec(
+        initial_rf_iso[0:3, 3], np.zeros(3), mid_rf_iso[0:3, 3], mid_rf_vel,
+        swing_time)
+
+    rf_pos_curve_second_half = interpolation.HermiteCurveVec(
+        mid_rf_iso[0:3, 3], mid_rf_vel, final_rf_iso[0:3, 3], np.zeros(3),
+        swing_time)
+
+    rf_quat_curve = interpolation.HermiteCurveQuat(
+        util.rot_to_quat(initial_rf_iso[0:3, 0:3]), np.zeros(3),
+        util.rot_to_quat(final_rf_iso[0:3, 0:3]), np.zeros(3), swing_time)
+
+    return base_pos_curve, base_quat_curve, lf_pos_curve_first_half, lf_pos_curve_second_half, lf_quat_curve, rf_pos_curve_first_half, rf_pos_curve_second_half, rf_quat_curve
+
+
+def lowerbody_inverse_kinematics(base_pos, base_quat, lf_pos, lf_quat, rf_pos,
+                                 lf_quat, open_chain_joints_name_dict,
+                                 joint_screws_in_ee_at_home, ee_config_at_home,
+                                 nominal_sensor_data_dict):
+    """
+    param: open_chain_joints_name_dict (dict): left_leg, right_leg, left_arm, right_arm
+    param: joint_screws_in_ee_at_home (dict): left_leg, right_leg, left_arm, right_arm
+    param: ee_config_at_home (dict): left_leg, right_leg, left_arm, right_arm
+    """
+
+    return joint_pos, lf_ik_done, rf_ik_done
+
+
 #main loop
 if __name__ == "__main__":
-    pass
+
+    pb.connect(pb.GUI)
+    pb.resetDebugVisualizerCamera(cameraDistance=1.5,
+                                  cameraYaw=90,
+                                  cameraPitch=0,
+                                  cameraTargetPosition=[0, 0, 0.8])
+    ## sim physics setting
+    pb.setPhysicsEngineParameter(fixedTimeStep=DT, numSubSteps=1)
+    pb.setGravity(0, 0, -9.81)
+
+    ## robot spawn & initial kinematics and dynamics setting
+    pb.configureDebugVisualizer(pb.COV_ENABLE_RENDERING, 0)
+    robot = pb.loadURDF(cwd + "/robot_model/atlas/atlas.urdf",
+                        Config.INITIAL_BASE_JOINT_POS,
+                        Config.INITIAL_BASE_JOINT_QUAT,
+                        useFixedBase=False)
+
+    ground = pb.loadURDF(cwd + "/robot_model/ground/plane.urdf",
+                         useFixedBase=True)
+    pb.configureDebugVisualizer(pb.COV_ENABLE_RENDERING, 1)
+
+    nq, nv, na, joint_id, link_id, pos_basejoint_to_basecom, rot_basejoint_to_basecom = pybullet_util.get_robot_config(
+        robot, INITIAL_BASE_JOINT_POS, INITIAL_BASE_JOINT_QUAT,
+        PRINT_ROBOT_INFO)
+
+    ##prepare inverse kinematics
+    open_chain_joints_name_dict, joint_screws_in_ee, ee_config_at_home = dict(
+    ), dict(), dict()
+    ee_links_name_dict, base_links_name_dict = dict(), dict()
+
+    ########
+
+    ##robot initial config setting
+    set_initial_config(robot, joint_id)
+
+    ##robot joint and link dynamics setting
+    pybullet_util.set_joint_friction(robot, joint_id, 0)
+    pybullet_util.set_link_damping(robot, link_id, 0., 0.)
--- test/hermite_curve_test.py
+++ test/hermite_curve_test.py
@@ -45,43 +45,13 @@ if __name__ == "__main__":
     # plt.show()
 
     ## hermiteCurveVec Test
-    # p1 = np.array([1., 1., 1.])
-    # v1 = np.array([0., 0., 0.])
-    # p2 = np.array([3., 3., 3.])
-    # v2 = np.array([0., 0., 0.])
-    # duration = 2.
-
-    # curveVec = interpolation.HermiteCurveVec(p1, v1, p2, v2, duration)
-
-    # time_list = []
-    # pos_list, vel_list, acc_list = [], [], []
-    # N = 100
-    # for i in range(N):
-    # t = duration * i / N
-    # time_list.append(t)
-    # pos_list.append(curveVec.evaluate(t))
-    # vel_list.append(curveVec.evaluate_first_derivative(t))
-    # acc_list.append(curveVec.evaluate_second_derivative(t))
-
-    # figure, axes = plt.subplots(3, 3)
-    # for i in range(3):
-    # axes[0][i].plot(time_list, [pos[i] for pos in pos_list])
-    # axes[0][i].grid(True)
-    # axes[1][i].plot(time_list, [vel[i] for vel in vel_list])
-    # axes[1][i].grid(True)
-    # axes[2][i].plot(time_list, [acc[i] for acc in acc_list])
-    # axes[2][i].grid(True)
-
-    # plt.show()
-
-    ## hermiteCurveQuat
-    p1 = np.array([0., 0., 0., 1.])
+    p1 = np.array([1., 1., 1.])
     v1 = np.array([0., 0., 0.])
-    p2 = np.array([1., 0., 0., 0.])
+    p2 = np.array([3., 3., 3.])
     v2 = np.array([0., 0., 0.])
     duration = 2.
 
-    curveQuat = interpolation.HermiteCurveQuat(p1, v1, p2, v2, duration)
+    curveVec = interpolation.HermiteCurveVec(p1, v1, p2, v2, duration)
 
     time_list = []
     pos_list, vel_list, acc_list = [], [], []
@@ -89,34 +59,53 @@ if __name__ == "__main__":
     for i in range(N):
         t = duration * i / N
         time_list.append(t)
-        pos_list.append(curveQuat.evaluate(t))
-        vel_list.append(curveQuat.evaluate_ang_vel(t))
-        acc_list.append(curveQuat.evaluate_ang_acc(t))
+        pos_list.append(curveVec.evaluate(t))
+        vel_list.append(curveVec.evaluate_first_derivative(t))
+        acc_list.append(curveVec.evaluate_second_derivative(t))
 
-    figure_1, axes_1 = plt.subplots(4, 1)
-    for i in range(4):
-        axes_1[i].plot(time_list, [pos[i] for pos in pos_list])
-        axes_1[i].grid(True)
-
-    figure_2, axes_2 = plt.subplots(3, 1)
+    figure, axes = plt.subplots(3, 3)
     for i in range(3):
-        axes_2[i].plot(time_list, [vel[i] for vel in vel_list])
-        axes_2[i].grid(True)
-
-    figure_3, axes_3 = plt.subplots(3, 1)
-    for i in range(3):
-        axes_3[i].plot(time_list, [acc[i] for acc in acc_list])
-        axes_3[i].grid(True)
+        axes[0][i].plot(time_list, [pos[i] for pos in pos_list])
+        axes[0][i].grid(True)
+        axes[1][i].plot(time_list, [vel[i] for vel in vel_list])
+        axes[1][i].grid(True)
+        axes[2][i].plot(time_list, [acc[i] for acc in acc_list])
+        axes[2][i].grid(True)
 
     plt.show()
 
-    # figure, axes = plt.subplots(3, 3)
+    ## hermiteCurveQuat
+    # p1 = np.array([0., 0., 0., 1.])
+    # v1 = np.array([0., 0., 0.])
+    # p2 = np.array([1., 0., 0., 0.])
+    # v2 = np.array([0., 0., 0.])
+    # duration = 2.
+
+    # curveQuat = interpolation.HermiteCurveQuat(p1, v1, p2, v2, duration)
+
+    # time_list = []
+    # pos_list, vel_list, acc_list = [], [], []
+    # N = 100
+    # for i in range(N):
+    # t = duration * i / N
+    # time_list.append(t)
+    # pos_list.append(curveQuat.evaluate(t))
+    # vel_list.append(curveQuat.evaluate_ang_vel(t))
+    # acc_list.append(curveQuat.evaluate_ang_acc(t))
+
+    # figure_1, axes_1 = plt.subplots(4, 1)
+    # for i in range(4):
+    # axes_1[i].plot(time_list, [pos[i] for pos in pos_list])
+    # axes_1[i].grid(True)
+
+    # figure_2, axes_2 = plt.subplots(3, 1)
     # for i in range(3):
-    # axes[0][i].plot(time_list, [pos[i] for pos in pos_list])
-    # axes[0][i].grid(True)
-    # axes[1][i].plot(time_list, [vel[i] for vel in vel_list])
-    # axes[1][i].grid(True)
-    # axes[2][i].plot(time_list, [acc[i] for acc in acc_list])
-    # axes[2][i].grid(True)
+    # axes_2[i].plot(time_list, [vel[i] for vel in vel_list])
+    # axes_2[i].grid(True)
 
-    plt.show()
+    # figure_3, axes_3 = plt.subplots(3, 1)
+    # for i in range(3):
+    # axes_3[i].plot(time_list, [acc[i] for acc in acc_list])
+    # axes_3[i].grid(True)
+
+    # plt.show()
