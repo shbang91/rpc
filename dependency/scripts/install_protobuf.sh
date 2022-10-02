732bc6be76fbb90bb56332eee89038b01ad875db
shbang91 <bangsh0718@gmail.com>
2022-10-02 11:49:46 -0500

    updated protobuf installation script

---
+++ dependency/scripts/install_protobuf.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+PATH_PACKAGE=$(pwd)
+
+echo '# ==============================================================='
+echo '# installing protobuf'
+echo '# ==============================================================='
+
+if [ "$(uname)" == "Darwin" ]; then
+
+    brew install protobuf
+
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+
+    sudo apt-get install autoconf automake libtool curl make g++ unzip &&
+    cd $PATH_PACKAGE/../ &&
+    git clone https://github.com/protocolbuffers/protobuf.git &&
+    cd protobuf &&
+    git checkout tags/v3.18.0 &&
+    git submodule update --init --recursive &&
+    ./autogen.sh &&
+    ./configure &&
+    make -j4 &&
+    make check &&
+    sudo make install &&
+    sudo ldconfig
+
+fi
+
+cd $PATH_PACKAGE
