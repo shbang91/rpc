4cea68998ccd2639c58d0bf4c1610a8dd63bac6e
shbang91 <bangsh0718@gmail.com>
2023-01-13 00:51:43 -0600

    dracointerface getcommand function structure changed

--- controller/draco_controller/draco_interface.cpp
+++ controller/draco_controller/draco_interface.cpp
@@ -73,13 +73,15 @@ void DracoInterface::GetCommand(void *sensor_data, void *command_data) {
   if (count_ <= waiting_count_) {
     // for simulation without state estimator
     // se_->UpdateGroundTruthSensorData(draco_sensor_data);
-    se_->InitializeSensorData(draco_sensor_data);
+    se_->Initialize(draco_sensor_data);
     this->_SafeCommand(draco_sensor_data, draco_command);
 
   } else {
     // for simulation without state estimator
     // se_->UpdateGroundTruthSensorData(draco_sensor_data);
-    se_->UpdateSensorData(draco_sensor_data);
+    sp_->state_ == draco_states::kInitialize
+        ? se_->Initialize(draco_sensor_data)
+        : se_->Update(draco_sensor_data);
     ctrl_arch_->GetCommand(draco_command);
     interrupt_->ProcessInterrupt();
   }
