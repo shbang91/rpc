feae01555a1e7b6b866ab0a632656597035e53a8
Carlos Gonzalez <carlos.gonzalez@utexas.edu>
2023-02-10 14:33:05 -0600

    simply updated labels

--- plot/draco/load_draco_label_names.m
+++ plot/draco/load_draco_label_names.m
@@ -12,3 +12,5 @@ draco_joint_label = ["l\_hip\_ie", "l\_hip\_aa", "l\_hip\_fe", "l\_knee\_fe\_jp"
     "r\_hip\_ie", "r\_hip\_aa", "r\_hip\_fe", "r\_knee\_fe\_jp", "r\_knee\_fe\_jd", "r\_ankle\_fe", "r\_ankle\_ie", "r\_shoulder\_fe", "r\_shoulder\_aa", "r\_shoulder\_ie", "r\_elbow\_fe", "r\_wrist\_ps", "r\_wrist\_pitch" ];
 draco_lf_label = ["l\_hip\_ie", "l\_hip\_aa", "l\_hip\_fe", "l\_knee\_fe\_jp", "l\_knee\_fe\_jd", "l\_ankle\_fe", "l\_ankle\_ie"];
 draco_rf_label = ["r\_hip\_ie", "r\_hip\_aa", "r\_hip\_fe", "r\_knee\_fe\_jp", "r\_knee\_fe\_jd", "r\_ankle\_fe", "r\_ankle\_ie"];
+draco_wbc_cost_labels = ["com\_xy\_task", "com\_z\_task", "joint\_task", "lf\_force\_task", "lf\_ori\_task", "lf\_pos\_task", ...
+    "rf\_force\_task", "rf\_ori\_task", "rf\_pos\_task", "qddot\_regularization\_task", "torso\_ori\_task", "upper\_body\_task"];
