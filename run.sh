abef4440e7a980a51d47e090b6d30501a9c871d2
Mingyo Seo <mingyo@utexas.edu>
2023-02-22 00:54:07 -0600

    joint states recording

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
-RPC_TEST=test2
+RPC_TEST=test
 PATH_DRACO=/home/mingyo/Projects/draco3/rpc_mingyo
+rm /tmp/draco_*
+rm $PATH_DRACO/*mp4
 python3 $PATH_DRACO/simulator/pybullet/draco_test.py --mode=headless
 mkdir $PATH_DRACO/data/$RPC_TEST
 mv $PATH_DRACO/*mp4 $PATH_DRACO/data/$RPC_TEST
