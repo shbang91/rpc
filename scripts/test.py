ef62210281271b37d79c79dcc3ceb061cc5cd404
Mingyo Seo <mingyo@utexas.edu>
2023-04-05 17:34:03 -0500

    testing trimming video

--- scripts/test.py
+++ scripts/test.py
@@ -11,8 +11,13 @@ args = parser.parse_args()
 
 
 f = h5py.File(args.file_path, "r")
-for i in range(len(f['obs/stereo'])):
-    img = f['obs/rgb'][i]
+i = len(f['obs/rgb']) - 1
+while f['action/l_gripper'][i] == 0 and f['action/r_gripper'][i] == 0 and i > 0:
+    i -= 1
+i += 20
+print(i)
+for j in range(0, i):
+    img = f['obs/rgb'][j]
     img_ra = np.array(img, dtype=np.uint8)
     cv2.imshow('test', img)
     cv2.waitKey(100)
@@ -32,7 +37,8 @@ print('obs/act_global_rh_ori', f['obs/act_global_rh_ori'][40])
 print('global_base_pos', f['global_base_pos'][40])
 print('global_base_ori', f['global_base_ori'][40])
 
-print('local_rh_ori', (R.from_quat(f['global_base_ori'][30][[3, 0, 1, 2]]).inv() * R.from_quat(f['obs/act_global_rh_ori'][30][[3, 0, 1, 2]])).as_quat())
+print('local_rh_ori', (R.from_quat(f['global_base_ori'][30][[3, 0, 1, 2]]).inv(
+) * R.from_quat(f['obs/act_global_rh_ori'][30][[3, 0, 1, 2]])).as_quat())
 
 
 f = f['data/demo_0']
