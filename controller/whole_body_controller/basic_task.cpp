b75bed885a62e35279e6d599407e993d7789c829
shbang91 <bangsh0718@gmail.com>
2023-05-25 20:40:40 -0500

    yamls for standing up with IK only

--- controller/whole_body_controller/basic_task.cpp
+++ controller/whole_body_controller/basic_task.cpp
@@ -200,6 +200,7 @@ LinkOriTask::LinkOriTask(PinocchioRobotSystem *robot, int target_idx)
 }
 
 void LinkOriTask::UpdateOpCommand() {
+
   Eigen::Quaterniond des_quat(des_pos_[3], des_pos_[0], des_pos_[1],
                               des_pos_[2]);
 
@@ -245,6 +246,7 @@ void LinkOriTask::UpdateOpCommand() {
 }
 
 void LinkOriTask::UpdateOpCommand(const Eigen::Matrix3d &rot_world_local) {
+  rot_link_w_ = robot_->GetLinkIsometry(target_idx_).linear().transpose();
   Eigen::Quaterniond des_quat(des_pos_[3], des_pos_[0], des_pos_[1],
                               des_pos_[2]);
 
