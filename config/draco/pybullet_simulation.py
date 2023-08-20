8c4ba79410fafa55288b5c5d0e5f33b7f10a0b96
Carlos Gonzalez <carlos.gonzalez@utexas.edu>
2023-08-20 15:48:57 -0500

    updated URDF with kinematic and dynamic properties of curved ankle
    Updated, on both l_ and r_ankle_ie:
      * joint limits to be within +/- 30 deg, 0 being foot flat horizontally
      * STL files have origin at rotating axis, instead of base of robot
      * collision box covers only the foot, instead of additional height
      * moments of inertia updated w.r.t. CoM
      * contact frames updates with new height and lateral offset

--- config/draco/pybullet_simulation.py
+++ config/draco/pybullet_simulation.py
@@ -2,7 +2,7 @@ class Config(object):
     CONTROLLER_DT = 0.00125
     N_SUBSTEP = 1
 
-    INITIAL_BASE_JOINT_POS = [0, 0, 0.95 - 0.2]
+    INITIAL_BASE_JOINT_POS = [0, 0, 0.95 - 0.195]
     INITIAL_BASE_JOINT_QUAT = [0, 0, 0, 1]
     # INITIAL_BASE_JOINT_POS = [0., 0., 1.5 - 0.757]
     # INITIAL_BASE_JOINT_QUAT = [0., 0., 0.7071, 0.7071]
