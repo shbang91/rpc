7c055d0722cfbfbde465a924830278c6592f83bc
Carlos Gonzalez <carlos.gonzalez@utexas.edu>
2024-06-08 23:10:51 -0500

    created FoxgloveParameterSubscriber class, tested only on n_steps parameter

---
+++ UI/foxglove/client/parameter_subscriber.cpp
@@ -0,0 +1,59 @@
+#include "parameter_subscriber.hpp"
+
+FoxgloveParameterSubscriber::FoxgloveParameterSubscriber(std::unordered_map<std::string, int*> &parameters_map,
+                                                         const std::string url) {
+  next_sub_id_ = 0;
+
+  client_.setBinaryMessageHandler([&](const uint8_t* data, size_t dataLength) {
+      if (static_cast<foxglove::BinaryOpcode>(data[0]) != foxglove::BinaryOpcode::MESSAGE_DATA) {
+        return;
+      }
+
+      client_.getParameters({"n_steps"});
+  });
+
+  client_.setTextMessageHandler([&](const std::string& payload) {
+      const nlohmann::basic_json msg = nlohmann::json::parse(payload);
+      const auto& op = msg["op"].get<std::string>();
+      if (op != "advertise") {
+        if (op == "parameterValues") {
+          auto param_value = msg["parameters"].get<std::vector<foxglove::Parameter>>();
+          for (const auto& param : param_value) {
+            auto p_val = param.getValue().getValue<int64_t>();
+            std::cout << param.getName() << ": " << p_val << std::endl;
+            *parameters_map[param.getName()] = p_val;
+          }
+        }
+        return;
+      }
+
+      const auto channels = msg["channels"].get<std::vector<foxglove::Channel>>();
+      std::vector<std::pair<foxglove::SubscriptionId, foxglove::ChannelId>> subscribePayload;
+
+      for (const auto& channel : channels) {
+        const auto subId = next_sub_id_++;
+        subscribePayload.push_back({subId, channel.id});
+      }
+
+      if (!subscribePayload.empty()) {
+        client_.subscribe(subscribePayload);
+      }
+  });
+
+  const auto openHandler = [&](websocketpp::connection_hdl) {
+      std::cout << "Connected to " << url << std::endl;
+  };
+  const auto closeHandler = [&](websocketpp::connection_hdl) {
+      std::cout << "Connection closed" << std::endl;
+  };
+
+  client_.connect(url, openHandler, closeHandler);
+}
+
+FoxgloveParameterSubscriber::~FoxgloveParameterSubscriber() {
+  client_.close();
+}
+
+void FoxgloveParameterSubscriber::UpdateParameters() {
+  // add here parameters that need to be interpolated from current to desired value
+}
---
+++ UI/foxglove/client/parameter_subscriber.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <foxglove/websocket/websocket_client.hpp>
+#include <foxglove/websocket/websocket_notls.hpp>
+#include <foxglove/websocket/serialization.hpp>
+
+class FoxgloveParameterSubscriber {
+public:
+  FoxgloveParameterSubscriber(std::unordered_map<std::string, int*> &parameters_map,
+                              const std::string uri = "ws://localhost:8766");
+  ~FoxgloveParameterSubscriber();
+
+  void UpdateParameters();
+
+private:
+  foxglove::SubscriptionId next_sub_id_;
+  foxglove::Client<foxglove::WebSocketNoTls> client_;
+};
