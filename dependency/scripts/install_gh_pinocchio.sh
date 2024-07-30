f48bab26abd5aa8b019fafb9fa2fa39db48f859c
Carlos Gonzalez <carlos.gonzalez@utexas.edu>
2024-07-30 13:56:17 -0500

    add ihwbc test and clean pdebug printouts

--- dependency/scripts/install_gh_pinocchio.sh
+++ dependency/scripts/install_gh_pinocchio.sh
@@ -9,7 +9,6 @@ if [ "$(uname)" == "Darwin" ]; then
     brew tap gepetto/homebrew-gepetto
     brew install pinocchio
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    echo '------- Add robotpkg apt repository -------'
     sudo apt install -qqy lsb-release curl
     sudo mkdir -p /etc/apt/keyrings
     curl http://robotpkg.openrobots.org/packages/debian/robotpkg.asc \
@@ -17,26 +16,9 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/robotpkg.asc] http://robotpkg.openrobots.org/packages/debian/pub $(lsb_release -cs) robotpkg" \
          | sudo tee /etc/apt/sources.list.d/robotpkg.list
     sudo apt update
-    echo '------- Install Pinocchio -------'
     sudo apt install -qqy robotpkg-py3*-pinocchio
-    echo '------- Configure robotpkg variables -------'
-    export PATH=/opt/openrobots/bin:$PATH
-    export PKG_CONFIG_PATH=/opt/openrobots/lib/pkgconfig:$PKG_CONFIG_PATH
-    export LD_LIBRARY_PATH=/opt/openrobots/lib:$LD_LIBRARY_PATH
-    export PYTHONPATH=/opt/openrobots/lib/python3.9/site-packages:$PYTHONPATH # Adapt your desired python version here
-    export CMAKE_PREFIX_PATH=/opt/openrobots:$CMAKE_PREFIX_PATH
 else
     echo "[error] os not detected"
 fi
-
-#cd $PATH_PACKAGE/../ &&
-#git clone --recursive https://github.com/stack-of-tasks/pinocchio &&
-#cd pinocchio &&
-#git checkout master &&
-#mkdir build &&
-#cd build &&
-#cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local && make &&
-#sudo make install
-
 
 cd ${PATH_PACKAGE}
