013bf9e85e78ba67026ff2fe19d1ee4e6f3157ae
Carlos Gonzalez <carlos.gonzalez@utexas.edu>
2024-07-30 13:27:07 -0500

    install pinoocchio via robotpkg

--- dependency/scripts/install_gh_pinocchio.sh
+++ dependency/scripts/install_gh_pinocchio.sh
@@ -6,30 +6,34 @@ echo "# install Pinocchio for CI pipeline"
 echo '# ==============================================================='
 
 if [ "$(uname)" == "Darwin" ]; then
-    brew install dartsim --only-dependencies
+    brew tap gepetto/homebrew-gepetto
+    brew install pinocchio
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    sudo apt-get install build-essential cmake pkg-config git &&
-    sudo apt-get install libeigen3-dev libassimp-dev libccd-dev libfcl-dev libboost-regex-dev libboost-all-dev &&
-    sudo apt-get install libopenscenegraph-dev &&
-    sudo apt-get install libbullet-dev &&
-    sudo apt-get install libode-dev &&
-    sudo apt-get install libtinyxml2-dev &&
-    sudo apt-get install liburdfdom-dev &&
-    sudo apt-get install libxi-dev libxmu-dev freeglut3-dev &&
-    sudo apt-get install libopenscenegraph-dev &&
-    sudo apt-get install liboctomap-dev
+    sudo apt install -qqy lsb-release curl
+    sudo mkdir -p /etc/apt/keyrings
+    curl http://robotpkg.openrobots.org/packages/debian/robotpkg.asc \
+         | sudo tee /etc/apt/keyrings/robotpkg.asc
+    echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/robotpkg.asc] http://robotpkg.openrobots.org/packages/debian/pub $(lsb_release -cs) robotpkg" \
+         | sudo tee /etc/apt/sources.list.d/robotpkg.list
+    sudo apt update
+    sudo apt install -qqy robotpkg-py3*-pinocchio
+    export PATH=/opt/openrobots/bin:$PATH
+    export PKG_CONFIG_PATH=/opt/openrobots/lib/pkgconfig:$PKG_CONFIG_PATH
+    export LD_LIBRARY_PATH=/opt/openrobots/lib:$LD_LIBRARY_PATH
+    export PYTHONPATH=/opt/openrobots/lib/python3.9/site-packages:$PYTHONPATH # Adapt your desired python version here
+    export CMAKE_PREFIX_PATH=/opt/openrobots:$CMAKE_PREFIX_PATH
 else
     echo "[error] os not detected"
 fi
 
-cd $PATH_PACKAGE/../ &&
-git clone --recursive https://github.com/stack-of-tasks/pinocchio &&
-cd pinocchio &&
-git checkout master &&
-mkdir build &&
-cd build &&
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local && make &&
-sudo make install
+#cd $PATH_PACKAGE/../ &&
+#git clone --recursive https://github.com/stack-of-tasks/pinocchio &&
+#cd pinocchio &&
+#git checkout master &&
+#mkdir build &&
+#cd build &&
+#cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local && make &&
+#sudo make install
 
 
 cd ${PATH_PACKAGE}
