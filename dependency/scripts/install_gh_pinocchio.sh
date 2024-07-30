1fea127406e0d29ec9729bfb68f13288e6866669
carlosiglezb <97119009+carlosiglezb@users.noreply.github.com>
2024-07-30 13:38:12 -0500

    [debug] add messages during pinocchio install

--- dependency/scripts/install_gh_pinocchio.sh
+++ dependency/scripts/install_gh_pinocchio.sh
@@ -9,6 +9,7 @@ if [ "$(uname)" == "Darwin" ]; then
     brew tap gepetto/homebrew-gepetto
     brew install pinocchio
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    echo '------- Add robotpkg apt repository -------'
     sudo apt install -qqy lsb-release curl
     sudo mkdir -p /etc/apt/keyrings
     curl http://robotpkg.openrobots.org/packages/debian/robotpkg.asc \
@@ -16,7 +17,9 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/robotpkg.asc] http://robotpkg.openrobots.org/packages/debian/pub $(lsb_release -cs) robotpkg" \
          | sudo tee /etc/apt/sources.list.d/robotpkg.list
     sudo apt update
+    echo '------- Install Pinocchio -------'
     sudo apt install -qqy robotpkg-py3*-pinocchio
+    echo '------- Configure robotpkg variables -------'
     export PATH=/opt/openrobots/bin:$PATH
     export PKG_CONFIG_PATH=/opt/openrobots/lib/pkgconfig:$PKG_CONFIG_PATH
     export LD_LIBRARY_PATH=/opt/openrobots/lib:$LD_LIBRARY_PATH
