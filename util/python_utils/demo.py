2f57b280db9cb709c3c4c9cce293090869821e61
shbang91 <bangsh0718@gmail.com>
2024-08-07 01:04:31 -0500

    added python teleop scripts to publish the teleop command through Realsense T265 camera

---
+++ util/python_utils/demo.py
@@ -0,0 +1,117 @@
+import os
+import sys
+import numpy as np
+
+cwd = os.getcwd()
+sys.path.append(cwd)
+
+import util.python_utils.util as util
+
+
+def post_process_obs(data):
+    return data
+
+
+def post_process_action(pose_vec_cur, pose_vec_prv, mode='quat'):
+
+    pose_cur = np.eye(4)
+    pose_cur[:3, :3] = util.quat_to_rot(pose_vec_cur[3:])
+    pose_cur[:3, 3] = pose_vec_cur[:3]
+
+    pose_prv = np.eye(4)
+    pose_prv[:3, :3] = util.quat_to_rot(pose_vec_prv[3:])
+    pose_prv[:3, 3] = pose_vec_prv[:3]
+
+    # Convert to relative pose
+    delta_pose = np.linalg.inv(pose_prv) @ pose_cur
+    delta_pos = delta_pose[:3, 3]
+    if mode == 'quat':
+        delta_quat = util.rot_to_quat(delta_pose[:3, :3])
+    else:
+        delta_quat = util.rot_to_euler(delta_pose[:3, :3])
+
+    act_data = np.concatenate([delta_pos, delta_quat])
+
+    return act_data
+
+
+def reconstruct_pose(delta_pose_vec, pose_vec_prv, mode='quat'):
+
+    delta_pose = np.eye(4)
+    if mode == 'quat':
+        delta_pose[:3, :3] = util.quat_to_rot(delta_pose_vec[3:])
+    else:
+        delta_pose[:3, :3] = util.euler_to_rot(delta_pose_vec[3:])
+    delta_pose[:3, 3] = delta_pose_vec[:3]
+
+    pose_prv = np.eye(4)
+    pose_prv[:3, :3] = util.quat_to_rot(pose_vec_prv[3:])
+    pose_prv[:3, 3] = pose_vec_prv[:3]
+
+    # Convert to relative pose
+    pose = pose_prv @ delta_pose
+    pose_vec = np.zeros(7)
+    pose_vec[:3] = pose[:3, 3]
+    pose_vec[3:] = util.rot_to_quat(pose[:3, :3])
+
+    return pose_vec
+
+
+def post_process_actions(pose_data, mode='quat'):
+
+    # Convert to relative pose
+    pose_rot = np.zeros((pose_data.shape[0], 4, 4))
+    pose_rot[:, 0:3, 0:3] = util.quat_to_rot(pose_data[:, 3:])
+    pose_rot[:, 0:3, 3] = pose_data[:, :3]
+    pose_rot[:, 3, 3] = 1
+
+    sample_size = pose_rot.shape[0]
+
+    delta_quat = []
+    delta_rpy = []
+    delta_pos = []
+
+    for i in range(sample_size - 1):
+        delta_transform = np.linalg.inv(pose_rot[i]) @ pose_rot[i + 1]
+        delta_pos.append(delta_transform[:3, 3])
+        delta_quat.append(util.rot_to_quat(delta_transform[:3, :3]))
+        delta_rpy.append(util.rot_to_euler(delta_transform[:3, :3]))
+
+    delta_pos.append(np.zeros(3))
+    delta_quat.append(np.array([0, 0, 0, 1]))
+    delta_rpy.append(np.array([0, 0, 0]))
+
+    if mode == 'quat':
+        act_data = np.concatenate(
+            [np.array(delta_pos), np.array(delta_quat)], axis=1)
+    else:
+        act_data = np.concatenate(
+            [np.array(delta_pos), np.array(delta_rpy)], axis=1)
+    return act_data
+
+
+def reconstruct_poses(act_data, initial_pose):
+    """
+    Reconstruct pose from action data
+    :param act_data: action data
+    :param initial_pose: initial pose
+    :return: pose data
+    """
+    # initial_pose = np.eye(4)
+    pose_vec = np.zeros((act_data.shape[0], 7))
+    pose_rot = np.zeros((act_data.shape[0], 4, 4))
+    pose_vec[0] = initial_pose
+
+    pose_rot[0] = np.eye(4)
+    pose_rot[0, :3, :3] = util.quat_to_rot(initial_pose[3:])
+    pose_rot[0, :3, 3] = initial_pose[:3]
+
+    for i in range(act_data.shape[0] - 1):
+        delta_transform = np.eye(4)
+        delta_transform[:3, :3] = util.quat_to_rot(act_data[i, 3:])
+        delta_transform[:3, 3] = act_data[i, :3]
+        pose_rot[i + 1] = pose_rot[i] @ delta_transform
+        pose_vec[i + 1, :3] = pose_rot[i + 1, :3, 3]
+        pose_vec[i + 1, 3:] = util.rot_to_quat(pose_rot[i + 1, :3, :3])
+
+    return pose_vec
---
+++ util/python_utils/device/t265.py
@@ -0,0 +1,167 @@
+'''
+Project:
+    Data collection / Teleoprator
+
+Title:
+    T265 Driver
+
+Description
+    -read asynchronous data streaming of T265
+
+* Copyrighted by Mingyo Seo
+* Bonston Dynamics AI Institute, The University of Texas at Austin
+'''
+import sys
+import os
+
+cwd = os.getcwd()
+sys.path.append(cwd)
+
+# First import the library
+import pyrealsense2 as rs
+
+# Import OpenCV and numpy
+import numpy as np
+from multiprocessing import Array, Lock, Queue
+import ctypes
+import time
+
+from util.python_utils.device.base import Sensor
+
+TIMEOUT = 0.5
+HEIGHT = 848
+WIDTH = 800
+FPS = 10
+
+
+class T265(Sensor):
+
+    def __init__(self,
+                 img_stream=False,
+                 timeout=TIMEOUT,
+                 img_size=(HEIGHT, WIDTH)) -> None:
+
+        self._left_buffer = Queue()
+        self._right_buffer = Queue()
+        self._left_lock = Lock()
+        self._right_lock = Lock()
+        self._pos_data = Array(ctypes.c_double, 3)
+        self._rot_data = Array(ctypes.c_double, 4)
+        self._angvel_data = Array(ctypes.c_double, 3)
+        self._vel_data = Array(ctypes.c_double, 3)
+        self._acc_data = Array(ctypes.c_double, 3)
+
+        self._timeout = timeout
+        self._img_size = img_size
+        self._img_stream = img_stream
+
+        self._left_img = None
+        self._right_img = None
+
+        # Build config object and stream everything
+        self._cfg = rs.config()
+        self._cfg.enable_stream(rs.stream.pose)
+        if self._img_stream:
+            self._cfg.enable_stream(rs.stream.fisheye, 1)
+            self._cfg.enable_stream(rs.stream.fisheye, 2)
+
+        super().__init__("T265")
+
+    def _fn_init(self):
+
+        # Wait until the first frameset is available
+        while np.sum(self.rot**2) == 0:
+            pass
+
+    def _fn_proc(self):
+
+        # Declare RealSense pipeline, encapsulating the actual device and sensors
+        pipe = rs.pipeline()
+        pipe.start(self._cfg)
+        self._time_proc.value = 0
+        init_time = time.time()
+
+        while self._flag_proc.value:
+
+            cur_time = time.time() - init_time
+            if cur_time - self._time_proc.value < 1. / FPS:
+                continue
+            if cur_time - self._time_proc.value > self._timeout:
+                break
+            self._time_proc.value += 1. / FPS
+
+            frames = pipe.wait_for_frames()
+            pose = frames.get_pose_frame().get_pose_data()
+
+            if self._img_stream:
+
+                f1 = frames.get_fisheye_frame(1).as_video_frame()
+                f2 = frames.get_fisheye_frame(2).as_video_frame()
+
+                self._left_lock.acquire()
+                self._left_buffer.put(np.asanyarray(f1.get_data()))
+                self._left_lock.release()
+
+                self._right_lock.acquire()
+                self._right_buffer.put(np.asanyarray(f2.get_data()))
+                self._right_lock.release()
+
+            self._pos_data[:] = (pose.translation.x, pose.translation.y,
+                                 pose.translation.z)
+            self._rot_data[:] = (pose.rotation.x, pose.rotation.y,
+                                 pose.rotation.z, pose.rotation.w)
+            self._vel_data[:] = (pose.velocity.x, pose.velocity.y,
+                                 pose.velocity.z)
+            self._angvel_data[:] = (pose.angular_velocity.x,
+                                    pose.angular_velocity.y,
+                                    pose.angular_velocity.z)
+            self._acc_data[:] = (pose.acceleration.x, pose.acceleration.y,
+                                 pose.acceleration.z)
+        pipe.stop()
+
+    @property
+    def left(self):
+        if not self._left_buffer.empty():
+            self._left_lock.acquire()
+            self._left_img = self._left_buffer.get()
+            self._left_lock.release()
+        return self._left_img
+
+    @property
+    def right(self):
+        if not self._right_buffer.empty():
+            self._right_lock.acquire()
+            self._right_img = self._right_buffer.get()
+            self._right_lock.release()
+        return self._right_img
+
+    @property
+    def pos(self):
+        return np.array(self._pos_data)
+
+    @property
+    def rot(self):
+        return np.array(self._rot_data)
+
+    @property
+    def vel(self):
+        return np.array(self._vel_data)
+
+    @property
+    def angvel(self):
+        return np.array(self._angvel_data)
+
+    @property
+    def acc(self):
+        return np.array(self._acc_data)
+
+    @property
+    def b_img_stream(self):
+        return self._img_stream
+
+
+if __name__ == "__main__":
+    t265 = T265()
+    t265.start()
+    while True:
+        pass
